cd0ea0ce539bddea25121297eaa544c47ed3cdad
Aristedes Patelis <6256545@johnabbottcollege.net>
2025-12-16 00:42:29 -0500

    Begun implementation of ship selection arrows.

--- src/states/game/ShipSelectScreenState.js
+++ src/states/game/ShipSelectScreenState.js
@@ -1,6 +1,6 @@
 import Input from "../../../lib/Input.js";
 import State from "../../../lib/State.js";
-import { input, stateStack, context, CANVAS_WIDTH, CANVAS_HEIGHT, images, sounds, songs } from "../../globals.js";
+import { input, stateStack, context, CANVAS_WIDTH, CANVAS_HEIGHT, images, sounds, songs, timer } from "../../globals.js";
 import PlayState from "./PlayState.js";
 import FontName from "../../enums/assets/FontName.js";
 import Colour from "../../enums/assets/ColorName.js";
@@ -11,8 +11,12 @@ import Player from "../../entities/Player.js";
 import LevelTransitionState from "./LevelTransitionState.js";
 import SoundName from "../../enums/assets/SoundName.js";
 import SongName from "../../enums/assets/SongName.js";
+import Vector from "../../../lib/Vector.js";
 
 export default class ShipSelectScreenState extends State {
+    static SELECT_ARROW_BASE = 10;
+    static SELECT_ARROW_HEIGHT = 10;
+    
     constructor(background) {
         super();
 
@@ -25,6 +29,9 @@ export default class ShipSelectScreenState extends State {
         );
 
         this.currentShipIndex = 0;
+
+        this.leftArrowPosition = new Vector(0,0);
+        this.rightArrowPosition = new Vector(0,0);
     }
 
     update(dt) {
@@ -44,26 +51,28 @@ export default class ShipSelectScreenState extends State {
 		context.fillText('SELECT YOUR SHIP', CANVAS_WIDTH/2, CANVAS_HEIGHT/2 - 30);
 
         context.filter = 'brightness(0%)';
-        this.playerShipSprites[this.currentShipIndex].render(CANVAS_WIDTH/2 - Player.WIDTH/2 + 2, CANVAS_HEIGHT/2 - Player.HEIGHT + 7);
+        this.playerShipSprites[this.currentShipIndex].render(CANVAS_WIDTH/2 - Player.WIDTH/2 + 1, CANVAS_HEIGHT/2 - Player.HEIGHT + 8);
         context.filter = 'brightness(100%)';
-        this.playerShipSprites[this.currentShipIndex].render(CANVAS_WIDTH/2 - Player.WIDTH/2, CANVAS_HEIGHT/2 - Player.HEIGHT + 5);
+        this.playerShipSprites[this.currentShipIndex].render(CANVAS_WIDTH/2 - Player.WIDTH/2, CANVAS_HEIGHT/2 - Player.HEIGHT + 7);
 
-        context.font = `15px ${FontName.Pixellari}`;
+        context.font = `16px ${FontName.Pixellari}`;
 		context.textAlign = 'center';
         context.fillStyle = Colour.Black;
-        context.fillText(PlayerShipNames[this.currentShipIndex], CANVAS_WIDTH/2 + 1.5, CANVAS_HEIGHT/2 + 27);
+        context.fillText(PlayerShipNames[this.currentShipIndex], CANVAS_WIDTH/2 + 1, CANVAS_HEIGHT/2 + 32);
 		context.fillStyle = Colour.White;
-        context.fillText(PlayerShipNames[this.currentShipIndex], CANVAS_WIDTH/2 - 1.5, CANVAS_HEIGHT/2 + 25);
+        context.fillText(PlayerShipNames[this.currentShipIndex], CANVAS_WIDTH/2, CANVAS_HEIGHT/2 + 30);
 
 		context.font = `15px ${FontName.Binary}`;
 		context.textAlign = 'center';
         context.fillStyle = Colour.Black;
-		context.fillText('Press \'ENTER\' to start.', CANVAS_WIDTH/2 + 2, CANVAS_HEIGHT/2 + 47);
+		context.fillText('Press \'ENTER\' to start.', CANVAS_WIDTH/2 + 2, CANVAS_HEIGHT/2 + 52);
 		context.fillStyle = Colour.White;
-		context.fillText('Press \'ENTER\' to start.', CANVAS_WIDTH/2, CANVAS_HEIGHT/2 + 45);
+		context.fillText('Press \'ENTER\' to start.', CANVAS_WIDTH/2, CANVAS_HEIGHT/2 + 50);
+
+        this.renderSelectionArrows();
 	}
 
-	checkForCommands() {
+	async checkForCommands() {
 		if (input.isKeyPressed(Input.KEYS.ENTER)) {
             stateStack.pop();
             songs.stop(SongName.Menu);
@@ -74,6 +83,7 @@ export default class ShipSelectScreenState extends State {
             stateStack.pop();
 		}
         else if (input.isKeyPressed(Input.KEYS.ARROW_LEFT)) {
+            this.leftArrowPosition.y -= 3;
             sounds.play(SoundName.ShipSelect);
             this.currentShipIndex -= 1;
 
@@ -82,6 +92,7 @@ export default class ShipSelectScreenState extends State {
             }
         }
         else if (input.isKeyPressed(Input.KEYS.ARROW_RIGHT)) {
+            this.rightArrowPosition.y -= 3;
             sounds.play(SoundName.ShipSelect);
             this.currentShipIndex += 1;
 
@@ -89,5 +100,45 @@ export default class ShipSelectScreenState extends State {
                 this.currentShipIndex = 0;
             }
         }
+
+        await timer.wait(0.5).then(() => {
+            this.resetArrowPositions();
+        });
 	}
+
+    renderSelectionArrows() {
+		this.renderLeftArrow();
+        this.renderRightArrow();
+	}
+
+    renderLeftArrow() {
+        context.save();
+		context.translate(CANVAS_WIDTH/2 - 20, CANVAS_HEIGHT/2);
+		context.beginPath();
+		context.moveTo(this.leftArrowPosition.x, this.leftArrowPosition.y - ShipSelectScreenState.SELECT_ARROW_BASE/2);
+		context.lineTo(-ShipSelectScreenState.SELECT_ARROW_HEIGHT, this.leftArrowPosition.y);
+		context.lineTo(this.leftArrowPosition.x, this.leftArrowPosition.y + ShipSelectScreenState.SELECT_ARROW_BASE/2);
+        context.lineTo(this.leftArrowPosition.x, this.leftArrowPosition.y);
+		context.closePath();
+		context.fill();
+		context.restore();
+    }
+
+    renderRightArrow() {
+        context.save();
+		context.translate(CANVAS_WIDTH/2 + 20, CANVAS_HEIGHT/2);
+		context.beginPath();
+		context.moveTo(this.rightArrowPosition.x, this.rightArrowPosition.y - ShipSelectScreenState.SELECT_ARROW_BASE/2);
+		context.lineTo(ShipSelectScreenState.SELECT_ARROW_HEIGHT, this.rightArrowPosition.y);
+		context.lineTo(this.rightArrowPosition.x, this.rightArrowPosition.y + ShipSelectScreenState.SELECT_ARROW_BASE/2);
+        context.lineTo(this.rightArrowPosition.x, this.rightArrowPosition.y);
+		context.closePath();
+		context.fill();
+		context.restore();
+    }
+
+    resetArrowPositions() {
+        this.leftArrowPosition = new Vector(0,0);
+        this.rightArrowPosition = new Vector(0,0);
+    }
 }
